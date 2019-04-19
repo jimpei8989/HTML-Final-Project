267776feccba38b743091e8cf038fc465ac5e68d
Wu-Jun <wjpei@csie.ntu.edu.tw>
2019-04-19 23:48:08 +0800

    Add Linear Regression

---
+++ LinReg/train.py
@@ -0,0 +1,22 @@
+import os, sys, pickle
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+from Utils.util import *
+
+if __name__ == "__main__":
+    # The lucky number is the sha256sum of our team name!
+    lucky_num = int('fcf7297f5ed12f689932bddde16eb95343daeee556b6e57523a8237aa056c345', 16) % (2 ** 32)
+
+    dataDir = sys.argv[1]
+    modelPath = sys.argv[2]
+
+    Xtrain, Ytrain, Xtest = LoadAll(dataDir)
+    print('-> Data Loaded', file = sys.stderr)
+
+    reg = LinearRegression(normalize = True, n_jobs = 12)
+    reg.fit(Xtrain, Ytrain)
+    print('Training Score:', reg.score(Xtrain, Ytrain))
+
+    with open(modelPath, 'wb') as f:
+        pickle.dump(reg, f)
---
+++ LinReg/train.sh
@@ -0,0 +1,3 @@
+directory=$1
+
+python3 ${directory}/train.py data ${directory}/model.pkl
