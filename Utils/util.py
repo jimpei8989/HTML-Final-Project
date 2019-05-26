c27fe24bec21fd23042c87599c62ee827f24f6cf
andy94077 <andy94077@gmail.com>
2019-05-26 22:58:29 +0800

    Add load_model, save_model, revise Model.score

--- Utils/util.py
+++ Utils/util.py
@@ -1,3 +1,4 @@
+import pickle
 import numpy as np
 from sklearn.utils import shuffle
 
@@ -9,15 +10,64 @@ def LoadData(path):
         a = npf.f.arr_0
     return a
 
+def load_data(path):
+    return LoadData(path)
+
 # Given data directory, return X_train, Y_train, X_test
 def LoadAll(path = 'data/'):
     path = path if path[-1] == '/' else path + '/'
     return LoadData(path + 'X_train.npz'), LoadData(path + 'Y_train.npz'), LoadData(path + 'X_test.npz')
 
+def load_all(path='data/'):
+    return LoadAll(path)
+
+
+def load_model(path):
+    '''
+    Description
+        Load models from given path
+    Args
+        path: model path
+    Returns
+        model
+    '''
+    with open(path, 'rb') as f:
+        return pickle.load(f)
+
+
+def save_model(model, path):
+    '''
+    Description
+        Save models to given path
+    Args
+        path: model path
+    Returns
+        None
+    '''
+    with open(path,'wb') as f:
+        pickle.dump(model, path)
+
+def generate_csv(path_or_clf,X,save_path='./output.csv'):
+    '''
+    Args
+        path_or_clf: the path of model or model itself
+        X: 2D array
+        save_path: path to csv (default ./output.csv)
+    Return
+        None
+    '''
+    model=path_or_clf
+    if isinstance(path_or_clf,str):
+        model=load_model(path_or_clf)
+    np.savetxt(save_path,model.predict(X),delimiter=',')
+
 def DataSplit(X, Y):
     Xp, Yp = shuffle(X, Y, random_state = lucky_num)
     cut = int(Xp.shape[0] * 0.8)
     return Xp[:cut, :], Yp[:cut, :], Xp[cut:, :], Yp[:cut, :]
+
+def data_split(X,Y):
+    return DataSplit(X,Y)
 
 class Model:
     def __init__(self, *args):
@@ -41,9 +91,10 @@ class Model:
             X: 2D array (num * input_dim)
             Y: 2D array (num * output_dim)
         Returns
-            3-tuple (MSE, WMAE, NAE)
+            2-tuple (WMAE, NAE)
         '''
-        raise NotImplementedError
+        w=np.array([[300],[1],[200]])
+        return np.mean(np.abs(self.predict(X)-Y) @ w), np.mean(np.sum(np.abs(self.predict(X)-Y) / Y, axis=1))
 
     def predict(self, X, *args):
         '''
@@ -54,23 +105,4 @@ class Model:
         '''
         raise NotImplementedError
 
-    def load_model(self, directory_path):
-        '''
-        Description
-            Load models from given path
-        Args
-            directory_path: a directory ending with '/'
-        Returns
-            None
-        '''
-
-    def save_model(self, directory_path):
-        '''
-        Description
-            Save models to given path
-        Args
-            directory_path: a directory ending with '/'
-        Returns
-            None
-        '''
-
+        
