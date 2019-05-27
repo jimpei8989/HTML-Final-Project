d3fedc7473810a9f56c4338c945bf0570ad610ef
andy94077 <andy94077@gmail.com>
2019-05-28 00:58:54 +0800

    Add fitted bool in LinReg

--- LinReg/LinReg.py
+++ LinReg/LinReg.py
@@ -6,13 +6,21 @@ from Utils.util import *
 from Utils.ReduceDimension import reduce_dimension
 
 class LinReg(Model):
+    def __init__(self):
+        super().__init__()
+        self.fitted=False
+
     def fit(self, trainX, trainY, reduce_dim=-1):
         self.reduce_dim=reduce_dim
         if reduce_dim >= 0:
             trainX = reduce_dimension(trainX,reduce_dim)
         self.reg = LinearRegression(normalize=True, n_jobs=os.cpu_count()).fit(trainX, trainY)
+        self.fitted = True
         return self
+
     def predict(self, X):
+        if not self.fitted:
+            raise Exception('The model has not been fitted yet!')
         return self.reg.predict(X if self.reduce_dim < 0 else reduce_dimension(X,self.reduce_dim))
 
 if __name__ == "__main__":
@@ -22,8 +30,8 @@ if __name__ == "__main__":
     data_dir = sys.argv[1]
     model_path = sys.argv[2]
 
-    trainX, trainY = load_data(data_dir + '/X_train.npz'),load_data(data_dir+'/Y_train.npz')
-    # trainX, trainY, testX = LoadAll(data_dir)
+    # trainX, trainY = load_data(data_dir + '/X_train.npz'),load_data(data_dir+'/Y_train.npz')
+    trainX, trainY, testX = LoadAll(data_dir)
     # testX = LoadData(data_dir + '/X_test.npz')
     print('-> Data Loaded', file=sys.stderr)
 
@@ -31,10 +39,10 @@ if __name__ == "__main__":
         reg = load_model(model_path)
         print('Load Model')
     except FileNotFoundError:
-        reg = LinReg().fit(trainX, trainY)
+        reg = LinReg().fit(trainX, trainY,20)
         with open(model_path, 'wb') as f:
             pickle.dump(reg, f)
 
-    print('Training Score:', reg.score(trainX, trainY))
+    #print('Training Score:', reg.score(trainX, trainY))
 
-    # generate_csv(reg,testX[:, [0] + list(range(5000, testX.shape[1]))])
+    generate_csv(reg,testX)
