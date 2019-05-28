d36285cffac48b574bd20ad2c4ec93ab7db1852d
andy94077 <andy94077@gmail.com>
2019-05-28 17:10:04 +0800

    Add train.sh from ReduceDimension

---
+++ train.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+if [[ "$1" == '--help' ]]; then
+	echo 'Usage: ./train.sh PYTHON_MODULE DATA_DIR MODEL_PATH'
+	exit 0
+fi
+wd="$(dirname "$(readlink -f "$0")")"
+
+file_path="$(readlink -f "$1")"
+relative_file="${file_path#$wd/}"
+data_dir="$2"
+model_path="$3"
+
+cd "$wd"
+
+time python3 -m "$(sed 's![/\\]!.!g' <<<"$relative_file" | awk -F '.' 'BEGIN{OFS=FS}{NF--;print}')" "$data_dir" "$model_path"
