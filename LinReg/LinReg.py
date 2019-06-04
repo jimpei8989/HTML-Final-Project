d17aa19af306eda510827c698f3f7d2f93b62c41
andy94077 <andy94077@gmail.com>
2019-06-04 21:59:41 +0800

    Revise to _fit and _predict in Lin.py

--- LinReg/LinReg.py
+++ LinReg/LinReg.py
@@ -1,28 +1,21 @@
 import os, sys, pickle
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.decomposition import PCA
 
 from Utils.util import *
-from Utils.ReduceDimension import reduce_dimension, pca
+from Utils.ReduceDimension import reduce_dimension
 
 class LinReg(Model):
     def __init__(self):
         super().__init__()
-        self.fitted=False
-        self.func = None
 
-    def fit(self, trainX, trainY, *args):
-        if args:
-            self.func = args[0]
-            trainX = self.func(trainX,*args[1:])
+    def _fit(self, trainX, trainY):
         self.reg = LinearRegression(normalize=True, n_jobs=os.cpu_count()).fit(trainX, trainY)
-        self.fitted = True
         return self
 
-    def predict(self, X, *args):
-        if not self.fitted:
-            raise Exception('The model has not been fitted yet!')
-        return self.reg.predict(X if self.func is None else self.func(X,*args))
+    def _predict(self, X):
+        return self.reg.predict(X)
 
 if __name__ == "__main__":
     # The lucky number is the sha256sum of our team name!
@@ -31,8 +24,8 @@ if __name__ == "__main__":
     data_dir = sys.argv[1]
     model_path = sys.argv[2]
 
-    trainX, trainY = load_data(data_dir + '/X_train.npz'),load_data(data_dir+'/Y_train.npz')
-    #trainX, trainY, testX = LoadAll(data_dir)
+    #trainX, trainY = load_data(data_dir + '/X_train.npz'),load_data(data_dir+'/Y_train.npz')
+    trainX, trainY, testX = LoadAll(data_dir)
     # testX = LoadData(data_dir + '/X_test.npz')
     print('-> Data Loaded', file=sys.stderr)
 
@@ -40,11 +33,11 @@ if __name__ == "__main__":
         reg = load_model(model_path)
         print('Load Model')
     except FileNotFoundError:
-        reg = LinReg()
-        reg.fit(trainX, trainY,pca,reg)
-        with open(model_path, 'wb') as f:
-            pickle.dump(reg, f)
+        pca=load_model(os.path.join(data_dir,'pca_model100'))
+        print('PCA loaded')
+        reg = LinReg().fit(trainX, trainY, transform_args = pca)
+        save_model(reg, model_path)
 
-    print('Training Score:', reg.score(trainX, trainY))
+    #print('Training Score:', reg.score(trainX, trainY))
 
-    #generate_csv(reg,testX)
+    generate_csv(reg,testX)
