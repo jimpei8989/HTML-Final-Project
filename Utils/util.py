c94a90afd796b825c08e4a64da15c3b79a8a56ef
andy94077 <andy94077@gmail.com>
2019-06-04 21:56:47 +0800

    Revise util.py. Add FeatureExtraction class, use _fit and _predict from now on

--- Utils/util.py
+++ Utils/util.py
@@ -57,7 +57,7 @@ def save_model(model, path):
         None
     '''
     with open(path,'wb') as f:
-        pickle.dump(model, path)
+        pickle.dump(model, f)
 
 def generate_csv(path_or_clf,X,save_path='./output.csv'):
     '''
@@ -81,23 +81,62 @@ def DataSplit(X, Y):
 def data_split(X,Y):
     return DataSplit(X,Y)
 
+class FeatureExtraction:
+    def __init__(self, func=None):
+        self.func = func
+    def fit(self, X, *args):
+        return self
+    def transform(self, X, *args):
+        return self.func(X, *args)
+
 class Model:
     def __init__(self, *args):
-        pass
+        self.fitted = False
 
-    def fit(self, trainX, trainY, validX=None, validY=None, *args):
+    def _fit(self, trainX, trainY, *args, validX=None, validY=None):
         '''
         Args
             trainX: 2D array (num * input_dim)
             trainY: 2D array (num * output_dim)
             validX: 2D array (num * input_dim)
             validY: 2D array (num * output_dim)
+            args:   other argument
         Returns
-            List of tuples, indicating the three
+            self
         '''
         raise NotImplementedError
 
-    def score(self, X, Y, *args):
+    def fit(self, trainX, trainY, *args, transform_args=(), validX=None, validY=None):
+        '''
+        Args
+            trainX: 2D array (num * input_dim)
+            trainY: 2D array (num * output_dim)
+            validX: 2D array (num * input_dim)
+            validY: 2D array (num * output_dim)
+            args:   other arguments passed to _fit function
+            transform_args: tuple of the feature extracting function(object) and its arguments
+        Returns
+            self
+        '''
+        if transform_args:
+            if not isinstance(transform_args, tuple):
+                transform_args = (transform_args,) # convert to a tuple
+            if callable(transform_args[0]): # it is a function
+                self.featex = FeatureExtraction(transform_args[0])
+            else:
+                self.featex = transform_args[0]
+
+            if not hasattr(self.featex, 'fitted') or not self.featex.fitted:
+                self.featex.fit(trainX, *transform_args[1:])
+            trainX = self.featex.transform(trainX,*transform_args[1:])
+        
+        self.fitted = True
+        if validX is not None and validY is not None:
+            return self._fit(trainX, trainY, *args, validX=validX, validY=validY)
+        else:
+            return self._fit(trainX, trainY, *args)
+
+    def score(self, X, Y, *args, transform_args = ()):
         '''
         Args
             X: 2D array (num * input_dim)
@@ -106,16 +145,31 @@ class Model:
             2-tuple (WMAE, NAE)
         '''
         w=np.array([[300],[1],[200]])
-        abs_value=np.abs(self.predict(X)-Y)
+        abs_value=np.abs(self.predict(X, *args, transform_args = transform_args)-Y)
         return np.mean(abs_value @ w), np.sum(abs_value / Y) / X.shape[0]
 
-    def predict(self, X, *args):
+    def _predict(self, X, *args):
         '''
         Args
             X: 2D array (num * input_dim)
+            args:   other arguments
         Returns
             Y: 2D array (num * output_dim)
         '''
         raise NotImplementedError
 
+    def predict(self, X, *args, transform_args = ()):
+        '''
+        Args
+            X: 2D array (num * input_dim)
+            args:   other arguments passed to _predict function
+            transform_args: tuple of the feature extracting function(object) and its arguments
+        Returns
+            Y: 2D array (num * output_dim)
+        '''
+        if hasattr(self, 'fitted') and not self.fitted:
+            raise Exception('The model has not been fitted yet!')
+        if hasattr(self, 'featex'):
+            X = self.featex.transform(X, *transform_args)
+        return self._predict(X, *args)
         
